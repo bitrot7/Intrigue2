b236ba06e40412a9604a36b084b484480e02b569
Q <mkeating@oswego.edu>
2017-04-06 14:26:36 -0400

    adding base classes for a new idea on gameflow

---
+++ core/src/com/mk/intrigue/entity/component/BaseComponent.java
@@ -0,0 +1,5 @@
+package com.mk.intrigue.entity.component;
+
+public abstract class BaseComponent {
+	public boolean active = false;
+}
---
+++ core/src/com/mk/intrigue/object/BaseObject.java
@@ -0,0 +1,11 @@
+package com.mk.intrigue.object;
+/**
+ * 
+ * @author q
+ *
+ * What if gamepaly mechanics could be controlled by a boolean flag in components and objects that determines what is effected
+ * in the systems at any moment in time?  programming through flags?
+ */
+public abstract class BaseObject {
+	public boolean active = false;
+}
