cc0ef0dad997c06927fe8223e74776991b5943d4
Q <mkeating@oswego.edu>
2017-04-06 22:55:47 -0400

    despaghettify system

--- core/src/com/mk/intrigue/system/IntrigueMotionSystem.java
+++ core/src/com/mk/intrigue/system/IntrigueMotionSystem.java
@@ -41,61 +41,58 @@ public class IntrigueMotionSystem extends SystemDecorator {
 		angularImpulse.set(0,0,0);
 		tmp.set(0,0,0);
 	}
+	private void setMovementDirection(IntrigueActionsComponent actions) {
+		if(actions.isLeft()) {
+			impulse.x = 1;
+		}
+		else if(actions.isRight()) {
+			impulse.x = -1;
+		}
+		if(actions.isForward()) {
+			impulse.z = 1;
+		}
+		else if(actions.isBackward()) {
+			impulse.z=-1;
+		}
+		if(actions.isJump()) {
+			impulse.y = 1;
+		}
+		if(actions.isSprint() && actions.isForward()) {
+			impulse.scl(1.5f);
+		}
+		if(actions.isTurningLeft()) {
+			angularImpulse.y = 1;
+		}
+		else if(actions.isTurningRight()) {
+			angularImpulse.y = -1;
+		}
+		if(actions.isAiming()) {
+			angularImpulse.scl(universal_rotate*.2f);
+		}
+		else {
+			angularImpulse.scl(universal_rotate);
+		}
+	}
 	@Override
 	public void update(float delta) {
 		super.update(delta);
 		for(Integer i : internal) {
 			this.resetUtilVectors();
 			Entity2 g = Intrigue.mamaDukes.get(i);
-			IntrigueActionsComponent s = g.getActionsComponent();
-			
 			AtomicPhysicalObject a = g.getPhysicalComponent().getPhysicsBody();
 			Vector3 v1 = a.getRigidBody().getLinearVelocity();
-			//Vector3 v2 = a.getRigidBody().getAngularVelocity();
 			
-			
-			a.getRigidBody().setDamping(0f, 0f);
-			if(s.isLeft()) {
-				impulse.x = 1;
-			}
-			else if(s.isRight()) {
-				impulse.x = -1;
-			}
-			if(s.isForward()) {
-				impulse.z = 1;
-			}
-			else if(s.isBackward()) {
-				impulse.z=-1;
-			}
-			if(s.isJump()) {
-				impulse.y = 1;
-			}
-			if(s.isIdle() && v1.y > -9f) {
-				a.getRigidBody().setAngularVelocity(tmp);
-			}
+			this.setMovementDirection(g.getActionsComponent());
 			//changed my method for getting orientation.  thank you bt and ockham
 			impulse.scl(universal_push);
 			impulse = a.getRigidBody().getOrientation().transform(impulse);
 			
-			if(s.isSprint() && s.isForward()) {
-				impulse.scl(1.5f);
-			}
+			
 			a.getRigidBody().setLinearVelocity(impulse);
 			
-			if(s.isTurningLeft()) {
-				angularImpulse.y = 1;
-			}
-			else if(s.isTurningRight()) {
-				angularImpulse.y = -1;
-			}
+			
 			tmp.set(0,1,0);
 			a.getRigidBody().setAngularFactor(tmp);
-			if(s.isAiming()) {
-				angularImpulse.scl(universal_rotate*.2f);
-			}
-			else {
-				angularImpulse.scl(universal_rotate);
-			}
 			a.getRigidBody().setAngularVelocity(angularImpulse);
 			a.getRigidBody().setAngularFactor(tmp);
 			
